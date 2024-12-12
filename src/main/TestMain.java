0f526a7922d11922deda41e92ee7cfeb79d6704a
Sergey Filippovskikh <me@filippovskikh.ru>
2024-12-12 21:29:52 +0300

    Создание ветки для 9 спринта

--- src/main/TestMain.java
+++ src/main/TestMain.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-public class Main {
+public class TestMain {
 
     public static void main(String[] args) {
 
